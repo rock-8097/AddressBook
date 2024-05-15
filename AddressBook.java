13bd922b0a8cc549ad6285debc971f770417d3b6
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-15 18:00:59 +0530

    relation of more than one class

---
+++ AddressBook.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+    public List<Contact> getAllContacts() {
+        return contacts;
+    }
+}
---
+++ Contact.java
@@ -0,0 +1,38 @@
+class Contact {
+    // instance variable
+    private String name;
+    private int phone;
+    private String email;
+    //constructor
+    public Contact(String name, int phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+       
+}
