4eab500b825f6bcbd52975a2329757fb6e04619a
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-16 12:31:51 +0530

    Contact created

--- Main.java
+++ Main.java
@@ -1,52 +1,13 @@
-import java.util.Scanner;
-
 public class Main {
     public static void data(){
-        Scanner scan = new Scanner(System.in);
-        // new AddressBook
-        AddressBook abook = new AddressBook();
-        boolean addingContacts = true;
-
-        while (addingContacts) {
-            // abook.addContact(newConnection(scan));
-            System.out.println("\nChoose an option:");
-            System.out.println("1. Add another contact");
-            System.out.println("2. Update a contact");
-            System.out.println("3. Delete a contact");
-            System.out.println("Press any other key Exit");
-            String choice = scan.nextLine();
-            switch (choice) {
-                case "1":
-                    abook.addContact(newConnection(scan));
-                    break;                
-                case "2":
-                    System.out.print("Enter the name of the contact you want to update: ");
-                    String toupdate_name = scan.nextLine();
-                    Contact updated = newConnection(scan);
-                    abook.updateContact(toupdate_name, updated);
-                    break;
-                case "3":
-                    System.out.print("Enter the name of the contact you want to update: ");
-                    String todelete = scan.nextLine();
-                    abook.deleteContact(todelete);
-                    break;
-                default:
-                    addingContacts = false;
-                    break;
-            }
-            
-        }
-        abook.displayContacts();
+        Contact contact1 = new Contact("John Doe", "1234567890", "john@example.com");
+        toPrint(contact1);
     }
-    public static Contact newConnection(Scanner scanner){
-        System.out.println("Enter contact details:");
-            System.out.print("Name: ");
-            String name = scanner.nextLine();
-            System.out.print("Phone Number: ");
-            String phone = scanner.nextLine();
-            System.out.print("Email: ");
-            String email = scanner.nextLine();
-            return new Contact(name, phone, email);
+
+    public static void toPrint(Contact contact){
+        System.out.println("Name: " + contact.getName());
+        System.out.println("Phone: " + contact.getPhone());
+        System.out.println("Email: " + contact.getEmail());
     }
 
     public static void main(String[] args) {
